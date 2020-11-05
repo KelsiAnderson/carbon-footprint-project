10d5eefdcac144cf6c273736d44dae9a645951ac
vagrant <vagrant@vagrant.vm>
2020-11-05 04:21:33 +0000

    porgress on server.py

---
+++ seed_database.py
@@ -0,0 +1,18 @@
+"""script to seed the database"""
+
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import crud
+import model
+import server
+
+os.sysytem('dropcb ratings')
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+
