b7041ef837bbd9896ec9e67b51ccabfbd39a733d
vagrant <vagrant@vagrant.vm>
2020-11-09 23:13:52 +0000

    fixed bug in base.html and server.py

--- server.py
+++ server.py
@@ -54,5 +54,6 @@ def create_new_user():
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
-
+    connect_to_db(app)
     app.run(debug=True, host="0.0.0.0")
+   
