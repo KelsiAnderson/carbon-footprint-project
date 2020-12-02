dbbe47f394778126fd1368fea535c92882302f18
vagrant <vagrant@vagrant.vm>
2020-12-02 19:47:09 +0000

    tostify messages working

---
+++ static/js/loginPageMessage.js
@@ -0,0 +1,21 @@
+const loginAlert = document.querySelector('#log-in-existing-user');
+console.log("LOOK AT MEEEE", loginAlert)
+loginAlert.addEventListener('click', (evt) => {
+    console.log("HIIIII")
+  Toastify({
+    text: "Hi there! Thanks for thinking about your emissions!",
+    duration: 5000,
+    backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+    className: "info",
+  }).showToast();
+});
+
+const test = document.querySelector("#test");
+console.log("HI IM TEXT MESSAGE", test)
+test.addEventListener('click', (evt) => {
+  console.log("THIS IS TEST")
+  Toastify({
+    text: "HEY IM A TEST BUTTON",
+    duration: 5000,
+  }).showToast();
+});
--- static/js/popUpMessages.js
+++ static/js/popUpMessages.js
@@ -20,15 +20,3 @@ function randomMessages(){
 }
 
 
-const loginAlert = document.querySelector('#log-in-existing-user');
-console.log("LOOK AT MEEEE", loginAlert)
-loginAlert.addEventListener('click', (evt) => {
-    console.log("HIIIII")
-  Toastify({
-    text: "Hi there! Thanks for thinking about your emissions!",
-    duration: 5000,
-    backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
-    className: "info",
-  }).showToast();
-});
-
