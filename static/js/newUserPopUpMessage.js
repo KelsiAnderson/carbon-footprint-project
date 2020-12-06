4b4787377647810245395172e91645ed2d386641
vagrant <vagrant@vagrant.vm>
2020-12-06 19:26:32 +0000

    added new pop up toast message

---
+++ static/js/newUserPopUpMessage.js
@@ -0,0 +1,11 @@
+const loginAlert = document.querySelector('#log-in-new-user');
+console.log("LOOK AT MEEEE", loginAlert)
+loginAlert.addEventListener('click', (evt) => {
+    console.log("HIIIII")
+  Toastify({
+    text: "Hi there! Thanks for thinking about your emissions!",
+    duration: 5000,
+    backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+    className: "info",
+  }).showToast();
+});
